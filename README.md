6bcda9e28902d7f2cc7cdc47be8dbbc8d972e51c
GwendolinePinault <pinaultgwendoline@gmail.com>
2025-04-18 14:21:28 +0200

    config(installation front): Create React project and install Tailwind

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Nextera front-end
+
 ## Organisation du dépôt
+Projet front avec React et Tailwind, installation et serveur front géré avec Vite.
+
---
+++ src/pages/App.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const App: React.FunctionComponent = () => {
+  return (
+    <div className="bg-sky-950 w-full h-full">
+      <h1 className="text-white">Nextera</h1>
+    </div>
+  )
+}
