366ab2487e9a57444726b02e88144c3eb34145fe
GwendolinePinault <pinaultgwendoline@gmail.com>
2025-04-19 12:30:36 +0200

    feat(graphic chart): add default style for titles, input, label, nav link, button and card

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
-import { NavLink, useNavigate } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom";
 
 export const Header = () => {
   const navigate = useNavigate();
   return (
-    <header className="w-full flex bg-slate-850 border-b-1 border-sky-500 p-2 mb-2 flex justify-between items-center">
+    <header className="w-full flex border-b-1 border-sky-500 py-2 px-4 mb-2 flex justify-between items-center">
       <nav>
-        <NavLink to='/'>Accueil</NavLink>
+        <NavLink to='/' className="nav-link">Accueil</NavLink>
       </nav>
       {/* change router direction when connexion page is connect to Router */}
-      <button type="button" onClick={() => navigate('/')} className="nextera-button">Se connecter</button>
+      <button type="button" onClick={() => navigate('/')}>Se connecter</button>
     </header>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 export const Home = () => {
   return (
     <main className="p-4">
-      <h1 className="text-xl text-center">Bienvenue sur Nextera Blog !</h1>
+      <h1>Bienvenue sur Nextera Blog !</h1>
     </main>
   )
 }
