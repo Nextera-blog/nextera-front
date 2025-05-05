82d0fa74486571f0ea4973c95682b21d0bc4449b
GwendolinePinault <pinaultgwendoline@gmail.com>
2025-05-05 16:33:02 +0200

    feat(posts-list-page): add structure to posts list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,21 @@
 export const Home = () => {
+  const posts = [{article_id: 1, title: "Le minimalisme numérique", content: "Le mininimalisme numérique consiste à réduire la surcharge digitale...", creation_date: "2025-05-05T17:29:49+02:00", author_id: 2}];
+
   return (
     <main className="p-4 flex flex-col items-center">
       <h1 className="mb-10">Bienvenue sur Nextera Blog !</h1>
 
       <section className="w-7xl grid grid-cols-2 gap-12">
-        <article className="card">
-          <h2 className="card-title">Titre</h2>
-          <p className="p-2">Description (short)</p>
-          <h4>Auteur, date</h4>
-        </article>
+        {posts.map((post) => {
+          const date = post.creation_date.split('T')[0];
+
+          return <article className="card" key={post.article_id}>
+            <h2 className="card-title">{post.title}</h2>
+            <p className="p-2">{post.content}</p>
+            <h4>Auteur {post.author_id}, {date}</h4>
+          </article>
+        }
+        )}
       </section>
     </main>
   )
