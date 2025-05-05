92cb09a828a46777b2b961338d4d89a18deaab2c
GwendolinePinault <pinaultgwendoline@gmail.com>
2025-05-05 15:44:18 +0200

    create posts list style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 export const Home = () => {
   return (
-    <main className="p-4">
-      <h1>Bienvenue sur Nextera Blog !</h1>
+    <main className="p-4 flex flex-col items-center">
+      <h1 className="mb-10">Bienvenue sur Nextera Blog !</h1>
+
+      <section className="w-7xl grid grid-cols-2 gap-12">
+        <article className="card">
+          <h2 className="card-title">Titre</h2>
+          <p className="p-2">Description (short)</p>
+          <h4>Auteur, date</h4>
+        </article>
+      </section>
     </main>
   )
 }
