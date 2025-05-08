4946450ec412fb28b8afd110295f5df109f5c465
GwendolinePinault <pinaultgwendoline@gmail.com>
2025-05-08 12:48:59 +0200

    feat(page redaction post): form visual done responsive design add for style in redaction page and home page fix header responsive design update styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,10 @@ export const Header = () => {
   return (
     <header className="w-full flex border-b-1 border-sky-500 py-2  px-2 mb-2 flex justify-between items-center">
       <div className="flex items-center">
-        <img src="/nextera-logo.png" alt="Logo Nextera" className="w-10" />
-        <p className="pl-2 font-mono text-base">Nextera blog</p>
+        <img src="/nextera-logo.png" alt="Logo Nextera" className="w-10 mr-2" />
+        <p className="hidden md:inline md:text-base">Nextera blog</p>
       </div>
-      <nav className="">
+      <nav className="flex justify-center items-center">
         <NavLink to='/' className="nav-link">Accueil</NavLink>
         <NavLink to='/redaction-article' className="nav-link">Rédiger un article</NavLink>
       </nav>
--- src/pages/RedactionArticlePage.tsx
+++ src/pages/RedactionArticlePage.tsx
@@ -1,9 +1,26 @@
 export const RedactionArticlePage: React.FunctionComponent = () => {
   return (
-    <main className="grow">
+    <main className="grow flex flex-col items-center h-5/6 overflow-hidden">
       <h1>Rédiger un article</h1>
 
-      
+      <section className="card w-5/6 md:w-1/2 md:mt-2 mb-6 grow p-4 md:p-8 md:max-h-full">
+        <h2 className="text-center mb-6">Nouvel article</h2>
+        <form action="POST" className="flex flex-col h-full">
+          <label htmlFor="title self-start">Titre de l'article</label>
+          <input type="text" name="title" className="md:p-3" />
+
+          <label htmlFor="content">Contenu de l'article</label>
+          <textarea name="content" id="content" className="h-32 md:h-1/3 md:max-h-1/2 overflow-y-scroll"></textarea>
+
+          <label htmlFor="author_name">Nom de l'auteur</label>
+          <input type="text" className="md:p-3" />
+
+          <label htmlFor="date">Date de création</label>
+          <input type="date" name="creation_date" id="creation_date" defaultValue={`${Date.now}`} className="md:p-3" />
+
+          <button className="self-center md:mb-3">Valider</button>
+        </form>
+      </section>
     </main>
-  )
-}
+  );
+};
