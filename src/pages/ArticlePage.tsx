92743ea65bd8ddeed083e7a0651f720c67c945d5
Polodys <136794468+Polodys@users.noreply.github.com>
2025-05-22 14:19:12 +0200

    First commit : types and basic commentaries display

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,33 +1,60 @@
-import { Link, useParams } from 'react-router-dom';
-import { Article } from '../types/api';
-import { getArticleById } from '../services/articles';
-import useFetch from '../hooks/useFetch';
-import DataFetchingState from '../components/DataFetchingState';
+import { Link, useParams } from "react-router-dom";
+import { Article } from "../types/api";
+import { getArticleById } from "../services/articles";
+import useFetch from "../hooks/useFetch";
+import DataFetchingState from "../components/DataFetchingState";
 
 export const ArticlePage: React.FunctionComponent = () => {
   const { id } = useParams();
-  const { loading, error, data: article } = useFetch<Article>(getArticleById, id);
+  const {
+    loading,
+    error,
+    data: article,
+  } = useFetch<Article>(getArticleById, id);
 
-  if (article) {
-    // const creationDate = article.creation_date.split('T')[0];
-    console.log("Article : ", article);
-    console.log("Article avant rendu :", article);
-    console.log("Article.author avant rendu :", article?.author);
-  }
+  // if (article) {
+  //   // const creationDate = article.creation_date.split('T')[0];
+  //   console.log("Article : ", article);
+  //   console.log("Article avant rendu :", article);
+  //   console.log("Article.author avant rendu :", article?.author);
+  // }
 
   return (
     <DataFetchingState loading={loading} error={error}>
       <main className="p-4 flex flex-col items-center grow h-full overflow-hidden">
         {article ? (
-          <section className='card grow w-1/2 m-6 overflow-y-auto-scroll flex flex-col'>
-            <h1 className='card-title'>{article.title}</h1>
-            <p className="whitespace-pre-wrap py-8 mx-8 my-4 border-y-2 border-sky-600 grow">{article.content}</p>
-            <p className="my-4 mr-8 text-end">Publié le {article.creation_date.split('T')[0]} par <Link to={`/authors/${article.author.user}`}>{article.author.name}</Link></p>
-          </section>
+          <>
+            <section className="card grow w-1/2 m-6 overflow-y-auto-scroll flex flex-col">
+              <h1 className="card-title">{article.title}</h1>
+              <p className="whitespace-pre-wrap py-8 mx-8 my-4 border-y-2 border-sky-600 grow">
+                {article.content}
+              </p>
+              <p className="my-4 mr-8 text-end">
+                Publié le {article.creation_date.split("T")[0]} par{" "}
+                <Link to={`/authors/${article.author.user}`}>
+                  {article.author.name}
+                </Link>
+              </p>
+            </section>
+            <section className="card grow w-1/2 m-6 overflow-y-auto-scroll flex flex-col bg-sky-950 border-sky-50 border-1 text-sky-50">
+              Commentaires           
+              {article.comments && article.comments.length > 0 && (
+                  article.comments.map(comment => (
+                    <div className="card bg-sky-950 border-sky-50 border-1 text-sky-50">
+                      <p>@{comment.user.name} . {comment.creation_date.split('T')[0]}</p>
+                      <p>{comment.content}</p>
+                    </div>
+                  ))
+                )}
+            </section>            
+          </>
         ) : (
-          !loading && !error && (
+          !loading &&
+          !error && (
             <main className="p-4 flex justify-center items-center grow">
-              <p className="text-red-500 mb-4 bg-red-50 p-2 rounded-md">Article non trouvé.</p>
+              <p className="text-red-500 mb-4 bg-red-50 p-2 rounded-md">
+                Article non trouvé.
+              </p>
             </main>
           )
         )}
--- src/types/api.ts
+++ src/types/api.ts
@@ -1,24 +1,42 @@
 export type Author = {
   // To be checked with the backend. BaseAuthorsSerializer is used to serialize the author in the article list and details. However, the 'Authors' model has a user field. The primary key of the authors table is in fact a user foreign key to Django's User model; the identifier we see in article.author.user is the id of the django user associated with this author
-  author_id?: number; 
+  // author_id?: number; 
   user: number;
-  bio: string;
-  join_date: string;
-  name?: string;
+  name?: string; // ! TO CHECK
+  bio: string | null;
   profile_picture_url: string | null;
-  username: string;
+  join_date: string;
+  // username: string;
+}
+
+export type Comment = {
+  comment_id: number;
+  content: string;
+  creation_date: string;
+  update_date: string;
+  user: Author;
+  article: number, // TO SEE : not article_id ?
+  parent_comment: number | null;
+  comment_replies: Comment[];
+}
+
+export type Tag = {
+  tag_id: number;
+  name: string;
 }
 
 export type Article = {
   article_id: number;
   title: string;
   content: string;
-  author_id: number;
   creation_date: string;
+  update_date: string;
+  // author_id: number;
   author: Author;
+  tags: Tag[];
+  comments: Comment[];
 }
 
-// Below, I reproduce the order and titles of the fields defined in the backend (develop branch : c04b22e - Merge pull request #18 from Nextera-blog/feature/KAN-33/routes_authors - 2025-05-19)
 export type ArticleMinimal = {
   article_id: number;
   title: string;
@@ -27,8 +45,8 @@ export type ArticleMinimal = {
 export type AuthorDetails = {
   user: number;
   name: string;
-  bio: string;
-  profile_picture_url: string;
+  bio: string | null;
+  profile_picture_url: string | null;
   join_date: string;
   articles: ArticleMinimal[];
 }
